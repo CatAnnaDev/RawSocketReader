6f4baf02ee99bcc5b260e0bb3c317c9a7b11a113
PsykoDev <45910905+PsykoDev@users.noreply.github.com>
2021-09-04 07:05:55 +0200

    Add files via upload

---
+++ Source.c
@@ -0,0 +1,43 @@
+#define WIN32_LEAN_AND_MEAN
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string>
+#include <winsock2.h>
+
+void dump(const unsigned char* data_buffer, const unsigned int length);
+
+int main(void) {
+	int i, recv_length, fd;
+	u_char buffer[10000];
+	if ((fd = socket(AF_INET, SOCK_RAW, IPPROTO_TCP)) == -1)
+		perror("-1 Return \n");
+	for (i = 0; i < 3; i++) {
+		recv_length = recv(fd, buffer, 10000, 0);
+		printf("Got a %d byte packet\n", recv_length);
+		dump(buffer, recv_length);
+	}
+}
+
+void dump(const unsigned char* data_buffer, const unsigned int length) {
+	unsigned char byte;
+	unsigned int i, j;
+	for (i = 0; i < length; i++) {
+		byte = data_buffer[i];
+		printf("%20x ", data_buffer[i]);
+		if (((i % 16) == 15) || (i == length - 1)) {
+			for (j = 0; j < 15 - (i % 16); j++) {
+				printf(" ");
+				printf("| ");
+				for (j = (i - (i % 16)); j <= i; j++) {
+					byte = data_buffer[j];
+					if ((byte > 31) && (byte > 127))
+						printf("%c", byte);
+					else
+						printf(".");
+				}
+				printf("\n");
+			}
+		}
+	}
+}
